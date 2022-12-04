f192d71f52e85b9726d2fdf9d5562b249a06de46
yash sajwan <yashsajwan12345@gmail.com>
2022-12-05 03:24:46 +0530

    transitions removed

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
 
-        //scroll animation termination
-        var count=1;
+        // //scroll animation termination
+        // var count=1;
 
-        $(window).on('scroll', function() {
-            if(count===1){
-                if ($(window).scrollTop() >= $(
-                    ".contact").offset().top - window.innerHeight+300) {
-                    $(".hidden").removeClass("right1");
-                    $(".hidden").removeClass("left1");
-                    $(".hidden").removeClass("hidden");
-                    count=0;
-                }
-            }
-        });
+        // $(window).on('scroll', function() {
+        //     if(count===1){
+        //         if ($(window).scrollTop() >= $(
+        //             ".contact").offset().top - window.innerHeight+300) {
+        //             $(".hidden").removeClass("right1");
+        //             $(".hidden").removeClass("left1");
+        //             $(".hidden").removeClass("hidden");
+        //             count=0;
+        //         }
+        //     }
+        // });
 
     });
 
 
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Art", "Music","Coding","Chess","Football"],
+        strings: ["Art", "Music", "Coding", "Chess", "Football"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Art", "Music","Coding","Chess","Football"],
+        strings: ["Art", "Music", "Coding", "Chess", "Football"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -74,15 +74,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
@@ -90,20 +90,22 @@ $(document).ready(function(){
     });
 });
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        }
-        else{
-            entry.target.classList.remove('show');
-        }
-    });
-});
+// scrolling transition
 
-const hiddenElements= document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         }
+//         else{
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const hiddenElements= document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
 
 
 
